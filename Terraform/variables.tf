9a808e6adb109fa4cb561a738e236f1def662b95
Arun Kumar <arukum@microsoft.com>
2019-10-09 18:18:09 -0500

    This Terraform is working and is Deploying all resources

---
+++ Terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "location" {
+  default = "southcentralus"
+}
+variable "resource_group_name" {
+  default = "kubernetes"
+}
+variable "adress_prefix" {
+  default = "10.240.0.0/24"
+}
+variable "port_ssh" {
+  default = "22"
+}
+variable "port_apiserver" {
+  default = "6443"
+}
+variable "count_controller" {
+  default = "3"
+}
+variable "count_worker" {
+  default = "3"
+}
+## These are the available parameters which are needed to deploy a 3X3 controlXworker infra
