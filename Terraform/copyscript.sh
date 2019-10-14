5f083bd033c51c2782a67e35baccf27930cd6225
Arun Kumar <38264681+arunkp89@users.noreply.github.com>
2019-10-14 11:19:26 -0500

    updated variables path

--- Terraform/copyscript.sh
+++ Terraform/copyscript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 terraform output|tr -d '[],=""'|tr '   ' '\n'|sed 's/controller-pip/[controlvms]/g'|sed 's/worker-pip/[datavms]/g'|tail -n +4 > ../Ansible/inventory/cluster
-echo
+echo -e \n
+echo "[all:vars]" >> ../Ansible/inventory/cluster
 echo azure_lb_pip=`terraform output azure_lb_pip` >> ../Ansible/inventory/cluster
