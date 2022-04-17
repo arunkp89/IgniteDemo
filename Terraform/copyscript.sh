53ac9853d52cd2eff2b29bcba44991a157c14ae5
Arun-MSFT <38264681+arunkp89@users.noreply.github.com>
2022-04-17 11:34:12 -0500

    Update copyscript.sh
    bash with ansible_ssh_user

--- Terraform/copyscript.sh
+++ Terraform/copyscript.sh
@@ -3,5 +3,9 @@ terraform output|tr -d '[],=""'|tr '   ' '\n'|sed 's/controller-pip/[controlvms]
 echo -e \n
 echo "[all:vars]" >> ../Ansible/inventory/cluster
 echo azure_lb_pip=`terraform output azure_lb_pip` >> ../Ansible/inventory/cluster
+echo "ansible_connection=ssh" >> ../Ansible/inventory/cluster
+echo "ansible_user=kuberoot" >> ../Ansible/inventory/cluster
+
+
 #ansible_ssh_user=kuberoot
 #ansible_ssh_private_key_file=/home/arun/clouddrive/.ssh/pkey.pem
